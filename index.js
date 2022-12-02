4ded5114f76eaaa9d0eed36d71a75135ca7e867e
loicsolbes <loic.solbes@gmail.com>
2022-12-02 11:57:17 +0100

    email verification

--- index.js
+++ index.js
@@ -12,11 +12,46 @@ const inputEl = document.querySelector('.input')
 
 const submitEl = document.getElementById('submit-btn')
 
+function ValidateEmail(input, el) {
+
+    var validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+    
+    if (input.value.match(validRegex)) {
+        return true;
+
+    } else {  
+        el.textContent = `Looks like this is not an email`
+        styling()
+}
+}
+
 function isEmpty(inputElement, element, message) {
     if(!inputElement.value) {
         inputElement.style.borderColor = 'hsl(0, 100%, 74%)';
         element.textContent = `${message} cannot be empty`
+        styling()
     }
+}
+
+function styling() {
+    const style = document.createElement('style');
+    style.innerHTML = `
+    label {
+        position: relative;
+      }
+      
+      label:before {
+        content: "";
+        position: absolute;
+        right: 10px;
+        top: 0;
+        bottom: 2.6rem;
+        width: 20px;
+        background: url(images/icon-error.svg) center / contain no-repeat;
+        backgroun
+      }
+    `;
+    document.head.appendChild(style);
 }
 
 submitEl.addEventListener('click', () => {
@@ -24,7 +59,6 @@ submitEl.addEventListener('click', () => {
     isEmpty(firstNameInputEl, firstNameEl, "First Name")
     isEmpty(lastNameInputEl, lastNameEl, "Last Name")
     isEmpty(passwordInputEl, passwordEl, "Password")
-
+    ValidateEmail(emailInputEl, emailEl)
 
 })
-
